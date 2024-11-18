a7d860f0776893a2bb0e6df440a5cb21deff3035
martins <martins@martinss-MacBook-Pro.local>
2024-11-18 21:05:58 +0100

    init

---
+++ challenge/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+public class StringSearchInFiles
+{
+    public void FindStringFiles(string folderPath, string searchString)
+    {
+
+
+        string[] files = Directory.GetFiles(folderPath);
+
+
+        //here we loop to search the files
+        foreach (string filePath in files)
+        {
+            // we read the content of the file
+            string fileContent = File.ReadAllText(filePath);
+            if (fileContent.Contains(searchString))
+            {
+
+                Console.WriteLine($"Present: {Path.GetFileName(filePath)}");
+            }
+            else
+            {
+                Console.WriteLine($"Absent: {Path.GetFileName(filePath)}");
+
+            }
+        }
+    }
+
+
+    public class Program
+    {
+        public static void Main()
+        {
+            //example search string in file
+            string searchForString = "testing";
+            string SearchfolderPath = @"C:\users\martins\desktop";  // put your directory name here
+          
+
+            StringSearchInFiles stringSearchfiles = new StringSearchInFiles();
+            stringSearchfiles.FindStringFiles(SearchfolderPath, searchForString);
+        }
+    }
+} 
